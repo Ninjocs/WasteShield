7391ad89a011a4e12e1e17aac08e5b0982cba6bc
Timothée LARDY etuEMSE <timothee.lardy@etu.emse.fr>
2025-01-02 14:59:12 +0100

    Trying to make the database work on the cloud

--- src/main/java/com/emse/spring/automacorp/AutomacorpApplication.java
+++ src/main/java/com/emse/spring/automacorp/AutomacorpApplication.java
@@ -2,14 +2,17 @@ package com.emse.spring.automacorp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.h2.tools.Server;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class AutomacorpApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		// Start the H2 server in TCP mode
+		Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
 
-
+		// Start the Spring Boot application
 		SpringApplication.run(AutomacorpApplication.class, args);
 	}
-
 }
