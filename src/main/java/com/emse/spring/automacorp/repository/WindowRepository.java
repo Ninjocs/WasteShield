d42005a9b1f000d0c869fc956d5bd3b786038397
Timothée LARDY etuEMSE <timothee.lardy@etu.emse.fr>
2025-01-10 13:29:54 +0100

    Cleaned the script and added annotations

--- src/main/java/com/emse/spring/automacorp/repository/WindowRepository.java
+++
@@ -1,8 +0,0 @@
-package com.emse.spring.automacorp.repository;
-
-import com.emse.spring.automacorp.model.WindowEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WindowRepository extends JpaRepository<WindowEntity, Long> {
-}
-
